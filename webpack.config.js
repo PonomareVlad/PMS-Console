a91baa8f11133184ae97e92bb8313c5901361ce4
PonomareVlad <public@ponomarevlad.ru>
2017-11-07 17:57:36 +0500

    Path fix

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
-        publicPath: '/',
+        publicPath: '/pms-console/',
         path: path.resolve(__dirname, './docs')
     },
     module: {
